4b5101efe764cfa63db5773521155f818d161b93
CarlosMatiaSSJ <carlosmatias649@gmail.com>
2023-08-05 21:28:17 -0600

    tipos alimentos añadidos en la migracion

--- database/migrations/2023_04_16_231923_tipos_alimentos.php
+++ database/migrations/2023_04_16_231923_tipos_alimentos.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -16,6 +17,13 @@ return new class extends Migration
             $table->string('descripcion');
 
         });
+
+        // Insertar los registros automáticamente
+        DB::table('tiposAlimentos')->insert([
+            ['descripcion' => 'alimento'],
+            ['descripcion' => 'bebida'],
+            ['descripcion' => 'snack'],
+        ]);
     }
 
     /**
